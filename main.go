7b7ea57aa5ee5a6b1571c1c7b2841d402b032791
Estevan Cantuaria <estevancantuaria@MacBook-Air-de-Estevan.local>
2025-07-04 17:05:26 -0300

    Testing argo cd local deploy

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+		fmt.Fprintf(w, "<h1>Hello, ArgoCD integration!</h1>")
 	})
 	http.ListenAndServe(":8080", nil)
 }
